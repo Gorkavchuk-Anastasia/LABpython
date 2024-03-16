a46e0e893bbcb4af39a2a027f3667f9ae39d2279
Gorkavchuk-Anastasia <151959543+Gorkavchuk-Anastasia@users.noreply.github.com>
2024-03-16 11:39:07 +0300

    Create dz05.py

---
+++ lab05/dz05.py
@@ -0,0 +1,28 @@
+import turtle
+turtle.shape("turtle")
+size = 100
+# нарисовали квадрат
+turtle.forward(size)
+turtle.right(90)
+turtle.forward(size)
+turtle.right(90)
+turtle.forward(size)
+turtle.right(90)
+turtle.forward(size)
+# нарисовали крышу
+turtle.right(45)
+turtle.forward(70)
+turtle.right(90)
+turtle.forward(70)
+turtle.right(45)
+# нарисовали дверь
+turtle.forward(size)
+turtle.right(90)
+turtle.forward(30)
+turtle.right(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(20)
+turtle.left(90)
+turtle.forward(50)
+turtle.exitonclick()
